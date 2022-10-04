4615ba9df9d0a10c860144f16065c23fa9f38689
leyle <leyle@leyle.com>
2022-10-04 12:32:54 +0800

    update callee format

--- pkg/crudlog/log.go
+++ pkg/crudlog/log.go
@@ -4,13 +4,33 @@ import (
 	"github.com/rs/zerolog"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var LogTarget = os.Stdout
 
+const (
+	gitlab = "gitlab"
+	github = "github"
+)
+
 func NewJsonLogger(level zerolog.Level) zerolog.Logger {
 	zerolog.SetGlobalLevel(level)
+
+	// Lshortfile
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		short := file
+		if strings.Contains(file, github) {
+			suffix := strings.Split(file, github)
+			short = suffix[len(suffix)-1][1:]
+		} else if strings.Contains(file, gitlab) {
+			suffix := strings.Split(file, gitlab)
+			short = suffix[len(suffix)-1][1:]
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
 	return zerolog.New(LogTarget).With().Timestamp().Caller().Logger()
 }
 
@@ -20,11 +40,12 @@ func NewConsoleLogger(level zerolog.Level) zerolog.Logger {
 	// Lshortfile
 	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
 		short := file
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				short = file[i+1:]
-				break
-			}
+		if strings.Contains(file, github) {
+			suffix := strings.Split(file, github)
+			short = suffix[len(suffix)-1][1:]
+		} else if strings.Contains(file, gitlab) {
+			suffix := strings.Split(file, gitlab)
+			short = suffix[len(suffix)-1][1:]
 		}
 		file = short
 		return file + ":" + strconv.Itoa(line)
